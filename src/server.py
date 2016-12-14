296e03b7df0756a571e38443e715f4f1689b0d44
Allyza Marquez <allyzamarquez@Allyzas-MacBook-Pro.local>
2016-12-13 20:09:20 -0800

    committing debugging of client server and test_client_server for debugging connectivity issues in tests.

--- src/server.py
+++ src/server.py
@@ -8,7 +8,7 @@ import sys
 def server():
     """Recieves a message from the client and echos it back."""
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    address = ("127.0.0.1", 5003)
+    address = ("127.0.0.1", 8093)
     server_socket.bind(address)
     server_socket.listen(1)
     conn, addr = server_socket.accept()
@@ -16,45 +16,49 @@ def server():
     message_complete = False
     response = ""
     while not message_complete:
-        if len(response) < buffer_length:
-            if response == "":
-                conn.sendall(response_error().encode('utf8'))
+        addition = conn.recv(buffer_length).decode('utf-8')
+        response += addition
+        print('r' + response)
+        print('a' + addition)
+        if response == 'fuck you.':
+            conn.sendall(response_error().encode('utf-8'))
             break
-        response += conn.recv(buffer_length).decode('utf8')
-    conn.close()
+        elif len(addition) < buffer_length:
+            break
     print(response)
-    conn.sendall(response_ok().encode('utf8'))
+    conn.sendall(response_ok().encode('utf-8'))
+    conn.close()
 
 
 def response_ok():
     """Returns 200 OK response"""
-    response = ("HTTP/1.1 200 OK\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\n" +
-        "Server: Teddy Bear\n" +
-        "Last-Modified: Wed, 22 Jul 1884 19:15:56 GMT\n" +
-        "Content-Length: 88\n" +
-        "Content-Type: text/html\n" +
-        "Connection: Closed\n" +
-        "<html>\n" +
-        "<body>\n" +
-        "<h1>Hello, World!</h1>\n" +
-        "</body>\n" +
+    response = ("HTTP/1.1 200 OK\r\n" +
+        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
+        "Server: Teddy Bear\r\n" +
+        "Last-Modified: Wed, 22 Jul 1884 19:15:56 GMT\r\n" +
+        "Content-Length: 88\r\n" +
+        "Content-Type: text/html\r\n" +
+        "Connection: Closed\r\n" +
+        "<html>\r\n" +
+        "<body>\r\n" +
+        "<h1>Hello, World!</h1>\r\n" +
+        "</body>\r\n" +
         "</html>")
     return response
 
 def response_error():
     """Returns 500 Internal Server Error response"""
-    response = ("HTTP/1.1 500 Internal Server Error\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\n" +
-        "Server: Teddy Bear\n" +
-        "Last-Modified: Wed, 22 Jul 1884 19:15:56 GMT\n" +
-        "Content-Length: 96\n" +
-        "Content-Type: text/html\n" +
-        "Connection: Closed\n" +
-        "<html>\n" +
-        "<body>\n" +
-        "<h1>Internal Server Error</h1>\n" +
-        "</body>\n" +
+    response = ("HTTP/1.1 500 Internal Server Error\r\n" +
+        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
+        "Server: Teddy Bear\r\n" +
+        "Last-Modified: Wed, 22 Jul 1884 19:15:56 GMT\r\n" +
+        "Content-Length: 96\r\n" +
+        "Content-Type: text/html\r\n" +
+        "Connection: Closed\r\n" +
+        "<html>\r\n" +
+        "<body>\r\n" +
+        "<h1>Internal Server Error</h1>\r\n" +
+        "</body>\r\n" +
         "</html>")
     return response
 
